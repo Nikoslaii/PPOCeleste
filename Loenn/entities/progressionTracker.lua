277055300db29b7e573ea79379f3141cb6e43ebd
Nikoslaii <96488137+Nikoslaii@users.noreply.github.com>
2025-11-30 18:26:53 +0100

    complément manque toujours node lua ondeath

--- Loenn/entities/progressionTracker.lua
+++ Loenn/entities/progressionTracker.lua
@@ -6,7 +6,6 @@ progressTracker.nodeLimits = {1, -1} -- garde les node entre 1 et l'infini
 
 progressTracker.fieldInformation = {
     flag = { fieldType = "string" }, -- le nom de notre entité
-    ordered = { fieldType = "boolean" } -- doit les passer dans l'ordre? (obsolète?)
 }
 
 progressTracker.placements = {
@@ -14,7 +13,6 @@ progressTracker.placements = {
         name = "progressTracker", -- valeur par défaut
         data = {
             flag = "progress_stage",
-            ordered = true
         }
     }
 }
@@ -25,7 +23,7 @@ progressTracker.nodeFieldInformation = { -- caractéristiques par node
 
 -- Défauts pour les nodes : tableau d'entrées (ici une entrée par défaut appliquée à tous les nodes)
 progressTracker.nodeDefaults = {
-    { onDeath = -1 }
+    { onDeath = 0 }
 }
 
 return progressTracker
